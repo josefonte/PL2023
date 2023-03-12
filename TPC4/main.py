6c14542c018d3a5c0d1be2564e4b7c174803ada9
José Fonte <73178551+josefonte@users.noreply.github.com>
2023-03-12 22:01:07 +0000

    Add files via upload

--- TPC4/main.py
+++ TPC4/main.py
@@ -4,23 +4,22 @@ import json
 f = open("./alunos.csv","r")
 c=0
 jsonOutput = []
-vault = dict()
 keys= list()
 
 for line in f:
     if c==0:
-        pattern = re.compile("(\w+)?{?(\w)?}?,+")   
+        pattern = re.compile("(\w+)?({?\w?}:*:*\w*)?,+")   
         header = pattern.findall(line)
         for item in header:
             key,extra = item
-            if extra : 
-                #er1 = re.compile("")   
-                vault[(key,extra)] = [] 
-            else:
-                vault[key] = [] 
+            if extra:
+                pat_extra = re.compile("{(\w-*\w*)}:*:*(\w*)")
+                extra_val = pat_extra.match(extra)
+                nValores, tipo = extra_val.groups()
+                nValores = int(nValores)
+                
             keys.append(key)
            
-
     else :
         pattern = re.compile("(\w+\s*\w*),?")
         values = pattern.findall(line)
@@ -28,14 +27,25 @@ for line in f:
                     keys[0]: values[0],
                     keys[1]: values[1],
                     keys[2]: values[2],
-                    keys[3]: []
                 }
-      
-        for item in values[3:]:
-            item = item.strip()
-            jitem[keys[3]].append(item)
-    
-        print(jitem)
+        
+        if tipo in ['media','sum']:
+            jitem[keys[3]+"_"+tipo] = 0 
+        else:
+            jitem[keys[3]] = []
+        
+        valor=0
+        
+        for i in range(nValores):
+            v = values[i+3].strip()
+            if tipo in ['media','sum']:
+                valor += int(v)
+                if i == int(nValores)-1 and tipo =='media':
+                    valor /= nValores    
+                jitem[keys[3]+"_"+tipo] = valor
+            else:
+                jitem[keys[3]].append(item)
+
         jsonOutput.append(jitem)
         
     c+=1
